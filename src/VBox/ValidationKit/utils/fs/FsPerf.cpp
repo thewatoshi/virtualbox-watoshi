ce532bf2dda7a3efe01eaef8db396c23b1d6b4ad
Knut St. Osmundsen <knut.osmundsen@oracle.com>
2025-07-09 17:21:13 +0000

    7.1: Backported r169604: ValKit/FsPerf: Limit the --many-files value to 2 million. bugref:10925
    svn:sync-xref-src-repo-rev: r169605

--- src/VBox/ValidationKit/utils/fs/FsPerf.cpp
+++ src/VBox/ValidationKit/utils/fs/FsPerf.cpp
@@ -1,4 +1,4 @@
-/* $Id: FsPerf.cpp 106320 2024-10-15 12:08:41Z klaus.espenlaub@oracle.com $ */
+/* $Id: FsPerf.cpp 110175 2025-07-09 17:21:13Z knut.osmundsen@oracle.com $ */
 /** @file
  * FsPerf - File System (Shared Folders) Performance Benchmark.
  */
@@ -6600,9 +6600,14 @@ int main(int argc, char *argv[])
 #undef CASE_OPT
 
             case kCmdOpt_ManyFiles:
-                g_fManyFiles = ValueUnion.u32 > 0;
-                g_cManyFiles = ValueUnion.u32;
-                break;
+                if (ValueUnion.u32 <= _256K * 8) /* Limit bitmap size to 256KB to avoid running out of stack. */
+                {
+                    g_fManyFiles = ValueUnion.u32 > 0;
+                    g_cManyFiles = ValueUnion.u32;
+                    break;
+                }
+                RTTestFailed(g_hTest, "Out of range --many-files value: %u (%#x)\n", ValueUnion.u32, ValueUnion.u32);
+                return RTTestSummaryAndDestroy(g_hTest);
 
             case kCmdOpt_NoManyFiles:
                 g_fManyFiles = false;
@@ -6713,7 +6718,7 @@ int main(int argc, char *argv[])
 
             case 'V':
             {
-                char szRev[] = "$Revision: 106320 $";
+                char szRev[] = "$Revision: 110175 $";
                 szRev[RT_ELEMENTS(szRev) - 2] = '\0';
                 RTPrintf(RTStrStrip(strchr(szRev, ':') + 1));
                 return RTEXITCODE_SUCCESS;
