2cb38d1247b5f0dbf8bdf0ba143f24456de16b06
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-07-15 17:00:16 +0000

    7.1: Backported r169668 (Audio/DrvHostAudioWasApi.cpp: Check if the device configuration in drvHostAudioWasCachePutBack() still is valid -- it might be not anymore if the IAudioClient reset failed for whatever reasons. Only needed if device caching is enabled. github:gh-72).
    svn:sync-xref-src-repo-rev: r169671

--- src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
+++ src/VBox/Devices/Audio/DrvHostAudioWasApi.cpp
@@ -1,4 +1,4 @@
-/* $Id: DrvHostAudioWasApi.cpp 109655 2025-05-26 07:52:19Z andreas.loeffler@oracle.com $ */
+/* $Id: DrvHostAudioWasApi.cpp 110232 2025-07-15 17:00:16Z andreas.loeffler@oracle.com $ */
 /** @file
  * Host audio driver - Windows Audio Session API.
  */
@@ -1484,6 +1484,8 @@ static int drvHostAudioWasCacheLookupOrCreate(PDRVHOSTAUDIOWAS pThis, IMMDevice 
  *
  * @param   pThis       The WASAPI host audio driver instance data.
  * @param   pDevCfg     The device config to put back.
+ *
+ * @note    Caller *must* check if pDevCfg still is valid. Can be NULL after return!
  */
 static void drvHostAudioWasCachePutBack(PDRVHOSTAUDIOWAS pThis, PDRVHOSTAUDIOWASCACHEDEVCFG pDevCfg)
 {
@@ -1514,7 +1516,8 @@ static void drvHostAudioWasCachePutBack(PDRVHOSTAUDIOWAS pThis, PDRVHOSTAUDIOWAS
     }
     else
     {
-        Log8Func(("IAudioClient::Reset failed (%Rhrc) on %p/'%s', destroying it.\n", hrc, pDevCfg, pDevCfg->szProps));
+        LogRelMax(64, ("WasAPI: IAudioClient::Reset failed (%Rhrc) on %p/'%s', destroying device config ...\n",
+                       hrc, pDevCfg, pDevCfg->szProps));
         drvHostAudioWasCacheDestroyDevConfig(pThis, pDevCfg);
     }
 }
@@ -2035,18 +2038,22 @@ static void drvHostAudioWasCompleteStreamDevSwitch(PDRVHOSTAUDIOWAS pThis, PDRVH
     /* Put the old config back into the cache. */
     drvHostAudioWasCachePutBack(pThis, pDevCfgOld);
 
-    AssertPtr(pDevCfgOld->pDevEntry);
-    if (pDevCfgOld->pDevEntry->fStale)
+    if (pThis->fCacheEnabled)
     {
-        LogRelFunc(("Removing stale device from cache\n"));
-        RTListNodeRemove(&pDevCfgOld->pDevEntry->ListEntry);
+        if (   pDevCfgOld /* Might be NULL if resetting failed in drvHostAudioWasCachePutBack() above. */
+            && pDevCfgOld->pDevEntry
+            && pDevCfgOld->pDevEntry->fStale)
+        {
+            LogRel2(("WasAPI: Removing stale device from cache\n"));
+            RTListNodeRemove(&pDevCfgOld->pDevEntry->ListEntry);
 
-        drvHostAudioWasCacheInvalidateDevEntryConfig(pThis, pDevCfgOld->pDevEntry);
-        drvHostAudioWasCacheDestroyDevEntry(pThis, pDevCfgOld->pDevEntry);
-        pDevCfgOld = NULL; /* Got invalid due to drvHostAudioWasCacheDestroyDevEntry() above. */
+            drvHostAudioWasCacheInvalidateDevEntryConfig(pThis, pDevCfgOld->pDevEntry);
+            drvHostAudioWasCacheDestroyDevEntry(pThis, pDevCfgOld->pDevEntry);
+            pDevCfgOld = NULL; /* Got invalid due to drvHostAudioWasCacheDestroyDevEntry() above. */
+        }
     }
 
-    LogFlowFunc(("returns with '%s' state: %s\n", pStreamWas->Cfg.szName, drvHostWasStreamStatusString(pStreamWas) ));
+    LogFlowFunc(("returns with '%s' state: %s\n", pStreamWas->Cfg.szName, drvHostWasStreamStatusString(pStreamWas)));
 }
 
 
