a9ee67bd33c950e79e12180fddff32c5c47026f3
Andreas Loeffler <andreas.loeffler@oracle.com>
2025-07-25 16:50:08 +0000

    7.1: Backported r169874 (Recording/Main: Check if there (already) is a progress object on failure in RecordingSettings::start()).
    svn:sync-xref-src-repo-rev: r169877

--- src/VBox/Main/src-server/RecordingSettingsImpl.cpp
+++ src/VBox/Main/src-server/RecordingSettingsImpl.cpp
@@ -1,4 +1,4 @@
-/* $Id: RecordingSettingsImpl.cpp 106320 2024-10-15 12:08:41Z klaus.espenlaub@oracle.com $ */
+/* $Id: RecordingSettingsImpl.cpp 110416 2025-07-25 16:50:08Z andreas.loeffler@oracle.com $ */
 /** @file
  *
  * VirtualBox COM class implementation - Machine capture settings.
@@ -409,9 +409,14 @@ HRESULT RecordingSettings::start(ComPtr<IProgress> &aProgress)
     if (RT_FAILURE(vrc))
     {
         /* Make the progress' error info available to the caller on failure. */
-        ComObjPtr<IVirtualBoxErrorInfo> pErrorInfo;
-        m->mProgress->COMGETTER(ErrorInfo)(pErrorInfo.asOutParam());
-        return setError(pErrorInfo);
+        if (m->mProgress.isNotNull()) /* Progress object available (yet)? */
+        {
+            ComObjPtr<IVirtualBoxErrorInfo> pErrorInfo;
+            m->mProgress->COMGETTER(ErrorInfo)(pErrorInfo.asOutParam());
+            return setError(pErrorInfo);
+        }
+
+        return setErrorBoth(VBOX_E_RECORDING_ERROR, vrc, "Starting recording failed with %Rrc", vrc);
     }
 
     return m->mProgress.queryInterfaceTo(aProgress.asOutParam());
